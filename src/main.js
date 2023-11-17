df076ce31cbaa36fae11aebdd4cac7fac5ca6e12
Yoojunghee <126542629+Yoojunghee@users.noreply.github.com>
2023-11-17 17:24:07 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
+// ✅ Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
 
 // getBoundingClientRect() 대신 offsetHeight 사용 가능. 단 offsetHeight은 소수점까지는 높이를 알려주지 않음
 
@@ -20,7 +20,7 @@ document.addEventListener('scroll', () => {
 
 /* ---------------------------------------------------- */
 
-// Home 섹션을 아래로 스크롤 시 투명하게 처리함
+// ✅ Home 섹션을 아래로 스크롤 시 투명하게 처리함
 // y가 0일때는 opacity 1 , y가 홈화면 높이에 완전히 닿았을때는 opacity 0
 // 홈화면 높이 구하기
 
@@ -39,8 +39,8 @@ document.addEventListener('scroll', () => {
 
 const arrowUp = document.querySelector('.arrow-up');
 
-// Arrow up버튼을 아래로 스크롤 시 투명하게 처리함
-// 홈이 절반정도 내려가면 위 버튼이 생기고 다시 스크롤 올리면 버튼 사라짐
+// ✅ Arrow up버튼을 아래로 스크롤 시 투명하게 처리함
+// 홈이 절반정도 내려가면 위 버튼이 생기고 다시 스크롤 올리면 버튼 사라짐)
 
 // 요소에 class를 추가하고 제거하면서 스타일링 변화 (좀 더 많은 걸 해야하면 class로)
 /* document.addEventListener('scroll', () => {
@@ -61,3 +61,24 @@ document.addEventListener('scroll', () => {
 });
 
 // 정밀하게 지정하지 않아도 되어 클래스 지정, 인라인 둘 다 가능
+
+/* ---------------------------------------------------- */
+
+// ✅ Navbar 토글버튼 클릭 처리
+// 화면이 작아지면 메뉴가 사라지면서 토글버튼 나타남 CSS로 처리함
+
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+/* ---------------------------------------------------- */
+
+// ✅ Navbar 메뉴 클릭 시 메뉴를 자동으로 닫아줌
+// 각 메뉴 클릭 시 페이지로 이동은 CSS로 처리함
+
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+});
